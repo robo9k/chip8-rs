00cc165769ebc02594bd8bb4c3b5385485013fe1
robo9k <robo@9k.lv>
2018-10-21 16:06:07 +0200

    Add Sys Instruction

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,8 +1,24 @@
 //! Machine language and byte code instructions
 
+/// Absolute memory address
+///
+/// Valid addresses are within `0x0` .. `0xFFF`.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Addr(u16);
+
+impl From<u16> for Addr {
+    fn from(bits: u16) -> Addr {
+        Addr(bits & 0x0FFF)
+    }
+}
+
 /// Byte code instruction
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Instruction {
+    /// Jumps to machine routine at `Addr`
+    ///
+    /// `0nnn` - `SYS addr`
+    Sys(Addr),
     /// Clears the display
     ///
     /// `00E0` - `CLS`
@@ -18,6 +34,7 @@ impl Instruction {
     pub fn decode(bits: u16) -> Option<Instruction> {
         use self::Instruction::*;
 
+        let nnn = bits & 0x0FFF;
         let high_nibble = (bits & 0xF000) >> 12;
         let k = bits & 0x00FF;
 
@@ -25,7 +42,7 @@ impl Instruction {
             0x0 => match k {
                 0xE0 => Some(Clear),
                 0xEE => Some(Return),
-                _ => None,
+                _ => Some(Sys(Addr(nnn))),
             },
             _ => None,
         }
@@ -45,5 +62,11 @@ mod tests {
     fn decode_return() {
         assert_eq!(Instruction::decode(0x00EE), Some(Instruction::Return));
     }
-
+    #[test]
+    fn decode_sys() {
+        assert_eq!(
+            Instruction::decode(0x0123),
+            Some(Instruction::Sys(Addr(0x0123)))
+        );
+    }
 }
