fb5976facd3acaaa8e6e0451c9da504a924af546
robo9k <robo@9k.lv>
2018-10-21 15:44:11 +0200

    Add Clear Instruction

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,11 +1,38 @@
 //! Machine language and byte code instructions
 
 /// Byte code instruction
-pub enum Instruction {}
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Instruction {
+    /// Clears the display
+    ///
+    /// `00E0` - `CLS`
+    Clear,
+}
 
 impl Instruction {
     /// Decodes raw `bits` into a valid `Instruction`
-    pub fn decode(_bits: u16) -> Option<Instruction> {
-        None
+    pub fn decode(bits: u16) -> Option<Instruction> {
+        use self::Instruction::*;
+
+        let high_nibble = (bits & 0xF000) >> 12;
+        let k = bits & 0x00FF;
+
+        match high_nibble {
+            0x0 => match k {
+                0xE0 => Some(Clear),
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_clear() {
+        assert_eq!(Instruction::decode(0x00E0), Some(Instruction::Clear));
     }
 }
