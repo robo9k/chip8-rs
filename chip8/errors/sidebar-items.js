c4f1cbd6135df2dc12139df9ae45705910ff02fa
robo9k <robo9k@symlink.io>
2020-05-21 19:01:29 +0000

    Deploying to gh-pages from  @ 43062814699e4bdf2c12723c713676dc2f8ddef9 🚀

--- chip8/errors/sidebar-items.js
+++ chip8/errors/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Chip8Error","Error type for all errors in this crate"]]});
+initSidebarItems({"enum":[["Chip8Error","Error type for all errors in this crate"]],"type":[["Result","Result alias"]]});
