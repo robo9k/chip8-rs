3eb184c4d773c482b7fe04ae1cacd0511137c934
robo9k <robo9k@symlink.io>
2020-05-21 18:21:12 +0200

    Pretty print tests and use macro

--- src/vm.rs
+++ src/vm.rs
@@ -7,6 +7,7 @@ use std::ops::{Index, IndexMut};
 pub type VRegisterValue = u8;
 
 /// General purpose registers
+#[derive(Debug)]
 pub struct VRegisters {
     vregisters: [VRegisterValue; 16], // TODO: Use variant_count for constant?
 }
@@ -90,6 +91,7 @@ impl IndexMut<VRegister> for VRegisters {
 }
 
 /// Virtual machine
+#[derive(Debug)]
 pub struct VM {
     registers: VRegisters,
 }
@@ -124,39 +126,74 @@ impl VM {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::instructions::{Instruction::*, VRegister::*};
 
     #[test]
     fn vregisters_set_get() {
         let mut registers = VRegisters::new();
-        registers[VRegister::V0] = 42;
-        assert_eq!(registers[VRegister::V0], 42);
+        println!("Created registers: {:?}", registers);
+
+        registers[V0] = 42;
+
+        assert_eq!(registers[V0], 42, "register value get equals set");
     }
 
-    #[test]
-    fn vm_execute_instruction_add() {
-        let mut vm = VM::new();
-        vm.registers[VRegister::V2] = 0xFE;
-        vm.registers[VRegister::V3] = 0x01;
+    macro_rules! registers_test {
+        (
+            $test_name:ident {
+                instruction: $instruction:expr,
+                registers_before: {$($register_before:expr => $register_before_value:expr),+},
+                registers_after: {$($register_after:expr => $register_after_value:expr),+},
+                register_overflow: $register_overflow_value:expr,
+            }
+        ) => {
+            #[test]
+            fn $test_name() {
+                let mut vm = $crate::vm::VM::new();
+                $(
+                  vm.registers[$register_before] = $register_before_value;
+                )+
+                println!("Created VM: {:?}", vm);
 
-        vm.execute_instruction(&Instruction::Add(VRegister::V2, VRegister::V3));
+                vm.execute_instruction(&$instruction);
+                println!("Executed instruction: {:?}", $instruction);
 
-        assert_eq!(vm.registers[VRegister::V2], 0xFF);
-        assert_eq!(vm.registers[VRegister::V3], 0x01);
-
-        assert_eq!(vm.registers[VRegister::VF], 0);
+                $(
+                    assert_eq!(
+                        vm.registers[$register_after],
+                        $register_after_value,
+                        "register value {:?} expected {}, actual {}",
+                        $register_after,
+                        $register_after_value,
+                        vm.registers[$register_after],
+                    );
+                )+
+                assert_eq!(
+                    vm.registers[$crate::instructions::VRegister::VF],
+                    $register_overflow_value,
+                    "overflow expected {}, actual {}",
+                    $register_overflow_value,
+                    vm.registers[$crate::instructions::VRegister::VF],
+                );
+            }
+        };
     }
 
-    #[test]
-    fn vm_execute_instruction_add_overflow() {
-        let mut vm = VM::new();
-        vm.registers[VRegister::V2] = 0xFF;
-        vm.registers[VRegister::V3] = 0x01;
+    registers_test!(
+        vm_execute_instruction_add {
+            instruction: Add(V2, V3),
+            registers_before: {V2 => 0xFE, V3 => 0x01},
+            registers_after: {V2 => 0xFF, V3 => 0x01},
+            register_overflow: 0,
+        }
+    );
 
-        vm.execute_instruction(&Instruction::Add(VRegister::V2, VRegister::V3));
-
-        assert_eq!(vm.registers[VRegister::V2], 0x00);
-        assert_eq!(vm.registers[VRegister::V3], 0x01);
-
-        assert_eq!(vm.registers[VRegister::VF], 1);
-    }
+    registers_test!(
+        vm_execute_instruction_add_overflow {
+            instruction: Add(V2, V3),
+            registers_before: {V2 => 0xFF, V3 => 0x01},
+            registers_after: {V2 => 0x00, V3 => 0x01},
+            register_overflow: 1,
+        }
+    );
 }
