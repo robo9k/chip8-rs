52fa3db540d12ba019926008ff1d483ded5b8ae1
robo9k <robo9k@symlink.io>
2020-05-22 10:20:25 +0000

    Deploying to gh-pages from  @ f308908e536116f0b6b7cb8d58e8f02c9c107d45 🚀

--- chip_8/vm/sidebar-items.js
+++ chip_8/vm/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["VM","Virtual machine"],["VRegisters","General purpose registers"]],"type":[["VRegisterValue","Type of a general purpose register in the VM"]]});
+initSidebarItems({"struct":[["VM","Virtual machine"]]});
