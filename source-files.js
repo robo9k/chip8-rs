6135de4341ab5c8fd545bf7e58ff519e3f6055cf
robo9k <robo9k@symlink.io>
2020-06-03 20:03:27 +0000

    Deploying to gh-pages from  @ da098a3421e0b8b154b9bea95c1ccd777f79f1e7 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["chip_8"] = {"name":"","files":["display.rs","errors.rs","instructions.rs","keypad.rs","lib.rs","memory.rs","vm.rs"]};
+sourcesIndex["chip_8"] = {"name":"","files":["display.rs","errors.rs","font.rs","instructions.rs","keypad.rs","lib.rs","memory.rs","vm.rs"]};
 createSourceSidebar();
